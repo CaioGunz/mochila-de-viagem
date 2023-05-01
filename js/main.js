3ea2cadfd5d956e1908ddf17fc2a704d45a3423f
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-01 16:59:07 -0300

    Removendo um Item

--- js/main.js
+++ js/main.js
@@ -24,18 +24,14 @@ form.addEventListener("submit", (evento) => {
 
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1 : 0;
 
         criarElemento(itemAtual)
 
         itens.push(itemAtual)
     }
-
-
-
-
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -54,9 +50,32 @@ function criarElemento(item) {
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    //remover um item do array
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    //escrever no localStorage
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
